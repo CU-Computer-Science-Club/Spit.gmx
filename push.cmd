2b6bb9b03c9b8be16f02d9d6603a372541155058
Heihachi <Gamehacker953@gmail.com>
2013-11-11 13:01:04 -0500

    [001] Fixing batch scripts

--- push.cmd
+++ push.cmd
@@ -3,6 +3,6 @@ ECHO Please make sure Git is set in the script or in your PATH Env Variable.
 ECHO Use carefully unwanted pushes or pushes that crash the game will be removed.
 pause
 ECHO Only for pushing to Concords CSC repo
-set /p user=Enter your username: 
-git push upstream user
+set /p branch=Enter the branch to push too: 
+git push upstream %branch%
 pause
--- set upstream.cmd
+++ set upstream.cmd
@@ -4,6 +4,6 @@ ECHO Use carefully unwanted pushes or pushes that crash the game will be removed
 pause
 ECHO Setting up Upstream
 git remote add upstream https://github.com/CU-Computer-Science-Club/Spit.gmx.git
-set /p user=Enter your username: 
-git branch user
+set /p branch=Enter the branch you want: 
+git branch %branch%
 pause
